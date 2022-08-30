dea5b54c893ea108cdab5f4564e5aa48a57a2102
hisham <hishamduke@gmail.com>
2022-08-30 18:18:30 +0530

    Text area to support

--- components/support.js
+++ components/support.js
@@ -1,18 +1,32 @@
-import Link from "next/link";
-import Breaking from "./breaking";
+import styles from "../styles/Support.module.css";
 export default function Support() {
+  let date = new Date();
+  function handlesubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
   return (
     <>
-      <div className="collumns">
-        <div className="dashboard">
-          <br />
+      <br />
+      <div className={styles.supportCont}>
+        <form
+          className={styles.form}
+          onSubmit={(e) => {
+            handlesubmit(e);
+          }}
+        >
+          <h2 className={"formhead"}>We are happy to help you.</h2>
+          <p>Please input in the following field :</p>
 
-          <form>
-            <h2 className={"formhead"}>We are happy to help you.</h2>
-          </form>
-        </div>
-        {/* <Breaking /> */}
+          <textarea
+            placeholder="type here ..."
+            className={styles.textarea1}
+          ></textarea>
+          <p></p>
+          <button> submit</button>
+        </form>
       </div>
+      <br />
     </>
   );
 }
