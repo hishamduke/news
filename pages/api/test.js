8c78710eb21339f57a3ff70ba666d79902a4879f
hisham <hishamduke@gmail.com>
2022-09-12 13:21:52 +0530

    middle

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,16 +1,6 @@
-import prisma from "../../lib/prisma";
-
-BigInt.prototype.toJSON = function () {
-  return this.toString();
-};
 export default async function handler(req, res) {
   try {
-    const userac = await prisma.User.findMany({
-      where: {
-        num: 1212121212,
-      },
-    });
-    res.status(200).json(userac);
+    res.status(200).json("HI");
   } catch (e) {
     console.log(e);
     res.status(200).json({ error: e.name });
