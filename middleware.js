a877389847d0f60d2859359be94ab04777d3ae32
hisham <hishamduke@gmail.com>
2022-09-12 13:17:22 +0530

    middle

--- middleware.js
+++ middleware.js
@@ -3,35 +3,35 @@ import { NextRequest, NextResponse } from "next/server";
 export const config = {
   // matcher: ["/dashboard/:path*", "/login/:path*", "/register"],
 };
-export async function middleware(request) {
-  let cookiename = "OurSiteJWT";
-  let cookie = cookiename + "=" + request.cookies.get(cookiename);
-  let url = request.nextUrl.origin + "/api/auth/cook";
-  let obj;
-  const valid = await fetch(url, {
-    headers: {
-      cookie: cookie,
-    },
-  });
-  console.log(request.nextUrl.pathname);
-  obj = await valid.json();
-  if (request.nextUrl.pathname.includes("/login")) {
-    if (obj.Message == !false) {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    } else {
-      return NextResponse.next();
-    }
-  }
-  if (request.nextUrl.pathname.includes("/register")) {
-    if (obj.Message == !false) {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    } else {
-      return NextResponse.next();
-    }
-  }
-  if (obj.Message == false) {
-    const logins = NextResponse.redirect(new URL("/login", request.url));
-    logins.cookies.set(cookiename, null, { maxAge: 0 });
-    return logins;
-  }
-}
+// export async function middleware(request) {
+//   let cookiename = "OurSiteJWT";
+//   let cookie = cookiename + "=" + request.cookies.get(cookiename);
+//   let url = request.nextUrl.origin + "/api/auth/cook";
+//   let obj;
+//   const valid = await fetch(url, {
+//     headers: {
+//       cookie: cookie,
+//     },
+//   });
+//   console.log(request.nextUrl.pathname);
+//   obj = await valid.json();
+//   if (request.nextUrl.pathname.includes("/login")) {
+//     if (obj.Message == !false) {
+//       return NextResponse.redirect(new URL("/dashboard", request.url));
+//     } else {
+//       return NextResponse.next();
+//     }
+//   }
+//   if (request.nextUrl.pathname.includes("/register")) {
+//     if (obj.Message == !false) {
+//       return NextResponse.redirect(new URL("/dashboard", request.url));
+//     } else {
+//       return NextResponse.next();
+//     }
+//   }
+//   if (obj.Message == false) {
+//     const logins = NextResponse.redirect(new URL("/login", request.url));
+//     logins.cookies.set(cookiename, null, { maxAge: 0 });
+//     return logins;
+//   }
+// }
