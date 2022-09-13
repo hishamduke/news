8095019bfbd61802e43381e5f61493e2c22179db
hisham <hishamduke@gmail.com>
2022-09-13 11:18:23 +0530

    logout

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   );
   const serialised = serialize("OurSiteJWT", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV != "development",
+    // secure: process.env.NODE_ENV != "development",
     sameSite: "strict",
     maxAge: -1,
     path: "/",
