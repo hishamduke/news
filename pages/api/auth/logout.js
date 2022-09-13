dcc69e8e0874ae49f637e649f8baafa9cc9066d1
hisham <hishamduke@gmail.com>
2022-09-13 11:40:34 +0530

    loggout for realz

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,24 +1,6 @@
-import prisma from "../../../lib/prisma";
-import { sign } from "jsonwebtoken";
-import { serialize } from "cookie";
-const secret = process.env.SECRET;
-export default async function handler(req, res) {
-  // console.log(secret);
+import { deleteCookie } from "cookies-next";
 
-  const token = sign(
-    {
-      exp: -1,
-    },
-    secret
-  );
-  const serialised = serialize("OurSiteJWT", token, {
-    httpOnly: true,
-    // secure: process.env.NODE_ENV != "development",
-    sameSite: "strict",
-    maxAge: -1,
-    path: "/",
-  });
-  // console.log("correct");
-  res.setHeader("Set-Cookie", serialised);
-  res.status(200).json({ Message: "logged out" });
+export default async function handler(req, res) {
+  deleteCookie("OurSiteJWT", { req, res });
+  return res.status(200).json({ message: "ok" });
 }
