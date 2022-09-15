1dfa2cfda9c01dbc46ea5e44162185cc95781435
hisham <hishamduke@gmail.com>
2022-09-15 08:37:54 +0530

    employee new and local db mysql

--- pages/dashboard/agent/employees.js
+++ pages/dashboard/agent/employees.js
@@ -2,6 +2,7 @@
 import styles from "../../../styles/Employees.module.css";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import Image from "next/image";
+import { useState } from "react";
 export default function employees() {
   return (
     <>
@@ -11,9 +12,12 @@ export default function employees() {
 }
 function Employee() {
   const [animationParent] = useAutoAnimate();
+  const [visible, setVisible] = useState(false);
+
   return (
-    <div className={styles.NewsCont} ref={animationParent}>
-      <>
+    <>
+      <NewEmp state={visible} action={setVisible} />
+      <div className={styles.NewsCont} ref={animationParent}>
         <div className={styles.NewsBox} ref={animationParent}>
           {/* <img className={styles.NewsImg} src="/newsboy.jpg" /> */}
           <h1 className={styles.NewsName}>Name</h1>
@@ -29,15 +33,27 @@ function Employee() {
           </p>
           <button className={styles.button}>Manage</button>
         </div>
-      </>
-      <>
+
         <div className={styles.NewsBox} ref={animationParent}>
           {/* <img className={styles.NewsImg} src="/newsboy.jpg" /> */}
           <h1 className={styles.NewsName}>Add a new employee</h1>
 
-          <button className={styles.button}>Add</button>
+          <button className={styles.button} onClick={() => setVisible(true)}>
+            Add
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+function NewEmp(prop) {
+  console.log(prop.action);
+  if (prop.state)
+    return (
+      <>
+        <div className={styles.fullscreen} onClick={() => prop.action(false)}>
+          hi
         </div>
       </>
-    </div>
-  );
+    );
 }
