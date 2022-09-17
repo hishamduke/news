1e3896fc081ee5a24d5b82ac569b11e61515e6ac
hisham <hishamduke@gmail.com>
2022-09-17 10:13:24 +0530

    random

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,6 @@ import Breaking from "../components/breaking";
 import Router from "next/router";
 export default function Home() {
   console.log(" logout mounted");
-
   return (
     <div className="collumns">
       <Login />
