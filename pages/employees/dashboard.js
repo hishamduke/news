198948291f0437c342c2b6dbe83b5cebf8141f49
hisham <hishamduke@gmail.com>
2022-12-15 11:42:39 +0530

    caching for emp

--- pages/employees/dashboard.js
+++ pages/employees/dashboard.js
@@ -17,62 +17,76 @@ export default function Dashboard(yea) {
     fetch("/api/employees/empdetails").then((res) => res.json())
   );
 
+  //caching
+  const {} = useQuery(["empSubs"], () =>
+    fetch("/api/employees/subscriptions").then((res) => res.json())
+  );
+  const {} = useQuery(["empdetails"], () =>
+    fetch("/api/employees/empdetails").then((res) => res.json())
+  );
+  const {} = useQuery(["empUsers"], () =>
+    fetch("/api/employees/empUsers").then((res) => res.json())
+  );
+  //endcaching
+
   if (isLoading) return "Loading...";
 
   if (error) return "An error has occurred: " + error.message;
 
   if (data) if (data.success == false) Router.push("/employees/login");
-  return (
-    <>
-      <div className="collumns">
-        {/* {JSON.stringify(data)} */}
-        <div
-          className="dashboard"
-          style={{ marginRight: "100px", gap: "10rem" }}
-        >
-          <br />
-          <form>
-            <h1 className={"formhead test"}>Hey {data.name} !</h1>
-            <h4>Users</h4>
-            <p
-              className={" Link"}
-              onClick={() => Router.push("/employees/users")}
-            >
-              View currently assigned Users
-            </p>
-            <p
-              className={" Link"}
-              onClick={() => Router.push("/employees/subscriptions")}
-            >
-              Subscriptions
-            </p>
-            <h4>Agents</h4>
-            <p
-              className={" Link"}
-              onClick={() => Router.push("/employees/writeagents")}
-            >
-              Write to Agents
-            </p>
-            <h4>Admin</h4>
-            <p className={" Link"} onClick={() => Router.push("/support")}>
-              Write to Admin for Support
-            </p>
-            <Logout />
-          </form>
-          <div style={{ margin: "auto" }}>
-            <Image
-              priority="false"
-              // placeholder="blur"
-              className="dashboard"
-              src="/kidnews.png"
-              height={"350"}
-              width={"350"}
+  {
+    return (
+      <>
+        <div className="collumns">
+          {/* {JSON.stringify(data)} */}
+          <div
+            className="dashboard"
+            style={{ marginRight: "100px", gap: "10rem" }}
+          >
+            <br />
+            <form>
+              <h1 className={"formhead test"}>Hey {data.name} !</h1>
+              <h4>Users</h4>
+              <p
+                className={" Link"}
+                onClick={() => Router.push("/employees/users")}
+              >
+                View currently assigned Users
+              </p>
+              <p
+                className={" Link"}
+                onClick={() => Router.push("/employees/subscriptions")}
+              >
+                Subscriptions
+              </p>
+              <h4>Agents</h4>
+              <p
+                className={" Link"}
+                onClick={() => Router.push("/employees/writeagents")}
+              >
+                Write to Agents
+              </p>
+              <h4>Admin</h4>
+              <p className={" Link"} onClick={() => Router.push("/support")}>
+                Write to Admin for Support
+              </p>
+              <Logout />
+            </form>
+            <div style={{ margin: "auto" }}>
+              <Image
+                priority="false"
+                // placeholder="blur"
+                className="dashboard"
+                src="/kidnews.png"
+                height={"350"}
+                width={"350"}
 
-              // style={{ transform: "translateY(-40px)" }}
-            ></Image>
+                // style={{ transform: "translateY(-40px)" }}
+              ></Image>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
