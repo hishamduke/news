f154c0379f0779766476ab0db17e612f0e9c8783
hisham <hishamduke@gmail.com>
2022-12-15 12:41:59 +0530

    newsFasterTouching

--- pages/api/news.js
+++ pages/api/news.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-const url =
-  "https://newsapi.org/v2/top-headlines?country=in&apiKey=" +
-  process.env.NEWSKEY;
+const add = "https://newsapi.org/v2/top-headlines?country=in&apiKey=";
+const url = add + process.env.NEWSKEY;
 export default async function handler(req, res) {
   await axios.get(url).then((res1) => {
     res.status(200).json(res1.data);
